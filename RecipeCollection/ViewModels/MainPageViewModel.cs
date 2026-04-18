2e7f25c3d6c3097230c4fa990a6833733fff2e30
Mattias Westman <mattiaswestman@outlook.com>
2026-04-18 16:24:23 +0200

    Added CollectionView that generates buttons based on ObservableCollection in MainPageViewModel.

--- RecipeCollection/ViewModels/MainPageViewModel.cs
+++ RecipeCollection/ViewModels/MainPageViewModel.cs
@@ -1,13 +1,21 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using RecipeCollection.Views;
+using System.Collections.ObjectModel;
 
 namespace RecipeCollection.ViewModels
 {
     public partial class MainPageViewModel : ObservableObject
     {
+        [ObservableProperty]
+        private ObservableCollection<string> categories;
+
         public MainPageViewModel() 
         {
+            Categories = new ObservableCollection<string>();
+            Categories.Add("Middag");
+            Categories.Add("Desert");
+            Categories.Add("Övrigt");
         }
 
         [RelayCommand]

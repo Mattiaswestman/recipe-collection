d2bca59d2a348fbd88c97f98ce15923bb8fd2414
Mattias Westman <mattiaswestman@outlook.com>
2026-04-22 14:02:57 +0200

    Added WIP Category & Recipe models.

---
+++ RecipeCollection/Models/Category.cs
@@ -0,0 +1,10 @@
+﻿
+namespace RecipeCollection.Models
+{
+    internal class Category
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public ICollection<Recipe> Recipes { get; set; } = new List<Recipe>();
+    }
+}
---
+++ RecipeCollection/Models/Recipe.cs
@@ -0,0 +1,12 @@
+﻿
+namespace RecipeCollection.Models
+{
+    internal class Recipe
+    {
+        public int Id { get; set; }
+        public int CategoryId { get; set; }
+        public string Title { get; set; }
+        public string Ingredients { get; set; }
+        public string Instructions { get; set; }
+    }
+}
